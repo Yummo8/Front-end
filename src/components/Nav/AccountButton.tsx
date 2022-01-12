8c0b9f96bf956d8a2439a1ab7c33ed0a6943c238
Yummo <soroslinuor@gmail.com>
2022-01-12 21:40:59 +0530

    feat: add ens name and avatar support

--- src/components/Nav/AccountButton.tsx
+++ src/components/Nav/AccountButton.tsx
@@ -4,6 +4,13 @@ import {useWallet} from 'use-wallet';
 import useModal from '../../hooks/useModal';
 import WalletProviderModal from '../WalletProviderModal';
 import AccountModal from './AccountModal';
+import {useENS} from '../../hooks/useENS';
+import Davatar from '@davatar/react';
+
+function shorten(str: string) {
+  if (str.length < 10) return str;
+  return `${str.slice(0, 6)}...${str.slice(str.length - 4)}`;
+}
 
 interface AccountButtonProps {
   text?: string;
@@ -12,6 +19,7 @@ interface AccountButtonProps {
 const AccountButton: React.FC<AccountButtonProps> = ({text}) => {
   const {account} = useWallet();
   const [onPresentAccountModal] = useModal(<AccountModal />);
+  const {ensName} = useENS(account);
 
   const [isWalletProviderOpen, setWalletProviderOpen] = useState(false);
 
@@ -33,7 +41,10 @@ const AccountButton: React.FC<AccountButtonProps> = ({text}) => {
         </Button>
       ) : (
         <Button onClick={onPresentAccountModal} className="shinyButtonSecondary">
-          My Wallet
+          <div className="account">
+            <Davatar size={20} address={account} />
+            <span>{ensName || shorten(account)}</span>
+          </div>
         </Button>
       )}
 
