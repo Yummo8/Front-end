ee2bcd3c2a25f12351b67ef9285dd948f51ac5c5
Yummo <soroslinuor@gmail.com>
2022-04-24 04:00:31 -0500

    change port 80

--- src/services/event.ts
+++ src/services/event.ts
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import {IResponse} from '../interfaces/IResponse';
 
-const baseUrl = "http://ec2-52-53-217-117.us-west-1.compute.amazonaws.com:5000/api/" || "http://localhost:5000/api/";
+const baseUrl = "http://ec2-52-53-217-117.us-west-1.compute.amazonaws.com/api/";
 export const eventApi = createApi({
   reducerPath: 'eventApi',
   baseQuery: fetchBaseQuery({
