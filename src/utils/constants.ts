3997c6dca4b07840fee68a334050706bdcb5442b
Yummo <soroslinuor@gmail.com>
2022-06-19 11:05:00 -0400

    update zapper address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,7 +9,7 @@ export const SW_ROUTER_ADDR = '0xC7f372c62238f6a5b79136A9e5D16A2FD7A3f0F5';
 // This is our zapper.
 export const ZAPPER_ROUTER_ADDR = '0x1f342d52e95b6953f32ef5eb0561aaead60c8cf9';
 // This is our zapper for swap sicle.
-export const SW_ZAPPER_ROUTER_ADDR = '0x6B99CBC724F81745c639615417Dd0B2e4740c8c1';
+export const SW_ZAPPER_ROUTER_ADDR = '0x15AA1C53d489beEA3BC639974F61e7021100FD2E';
 
 // Literally no purpose but keep it in so
 // like 500 files won't break.
