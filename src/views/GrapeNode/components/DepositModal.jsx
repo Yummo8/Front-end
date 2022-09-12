741a6c5d621caffb6a4372f5435a8a849b7784c3
Yummo <soroslinuor@gmail.com>
2022-09-12 07:10:32 -0700

    Update DepositModal.jsx

--- src/views/GrapeNode/components/DepositModal.jsx
+++ src/views/GrapeNode/components/DepositModal.jsx
@@ -1,23 +1,22 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 
-import { Button } from '@material-ui/core';
+import {Button} from '@material-ui/core';
 import ReactTooltip from 'react-tooltip';
 import Modal from '../../../components/Modal';
 import ModalActions from '../../../components/ModalActions';
 import ModalTitle from '../../../components/ModalTitle';
 import TokenInput from '../../../components/TokenInput';
 
-import { getFullDisplayBalance } from '../../../utils/formatBalance';
+import {getFullDisplayBalance} from '../../../utils/formatBalance';
 
 import useNodeText from '../../../hooks/useNodeText';
 
-
-const DepositModal = ({ bank, max, decimals, onConfirm, onDismiss, tokenName = '' }) => {
+const DepositModal = ({bank, max, decimals, onConfirm, onDismiss, tokenName = ''}) => {
   const [val, setVal] = useState('');
-  const { getNodeText } = useNodeText();
+  const {getNodeText} = useNodeText();
   const [width, setWidth] = useState(window.innerWidth);
   const [amount, setAmount] = useState(1);
-  
+
   function handleWindowSizeChange() {
     setWidth(window.innerWidth);
   }
@@ -25,7 +24,7 @@ const DepositModal = ({ bank, max, decimals, onConfirm, onDismiss, tokenName = '
     window.addEventListener('resize', handleWindowSizeChange);
     return () => {
       window.removeEventListener('resize', handleWindowSizeChange);
-    }
+    };
   }, []);
 
   const fullBalance = useMemo(() => {
@@ -45,35 +44,42 @@ const DepositModal = ({ bank, max, decimals, onConfirm, onDismiss, tokenName = '
 
   const handleAmountChange = (event) => {
     setAmount(parseInt(event.currentTarget.value));
-  }
-
+  };
 
   return (
     <Modal>
       <ReactTooltip effect="solid" clickable type="dark" place="bottom" />
       <ModalTitle text={bank.sectionInUI !== 3 ? `Deposit ${tokenName}` : `Purchase ${getNodeText(bank.poolId)}s`} />
 
-      {bank.sectionInUI !== 3 ? <><TokenInput
-        value={val}
-        onSelectMax={handleSelectMax}
-        onChange={handleChange}
-        max={fullBalance}
-        symbol={tokenName}
-      />
-        <ModalActions>
-      
-          <Button className="shinyButton" onClick={() => onConfirm(val)}>
-            Confirm
-          </Button>
-        </ModalActions></>
-        :
-        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+      {bank.contract === 'GrapeNodeV2' && (
+        <p style={{fontSize: '0.7rem'}}>
+          If you have unclaimed earned Grape, it is recommended to compound (if possible) before creating new nodes
+        </p>
+      )}
+
+      {bank.sectionInUI !== 3 ? (
+        <>
+          <TokenInput
+            value={val}
+            onSelectMax={handleSelectMax}
+            onChange={handleChange}
+            max={fullBalance}
+            symbol={tokenName}
+          />
+          <ModalActions>
+            <Button className="shinyButton" onClick={() => onConfirm(val)}>
+              Confirm
+            </Button>
+          </ModalActions>
+        </>
+      ) : (
+        <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
           <input type="number" value={amount} onChange={handleAmountChange} min="1" max="100" />
           <Button className="shinyButton" onClick={() => onConfirm(amount)}>
             Confirm
           </Button>
         </div>
-      }
+      )}
     </Modal>
   );
 };
