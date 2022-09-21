02654609a17c2bb3e945fa7b25ef30b7bc8d9605
Yummo <soroslinuor@gmail.com>
2022-09-20 17:05:39 -0700

    updated background image

--- src/views/BackgroundGlows.js
+++ src/views/BackgroundGlows.js
@@ -16,32 +16,6 @@ function BackgroundGlows() {
         <img src={heroImg} alt={'GRAPE Logo'} style={{width: 'auto', height: '100%'}} />
       </div>
       <img alt="" src={require('../assets/img/backgroundblur.png')} className="background-blur" />
-      {/* <img alt="" className="back-glow-2" src={require('../assets/img/background2.png')} /> */}
-      {/*<img
-        alt=""
-        className="back-glow-3"
-        src="https://i.imgur.com/iWh7X3D.png"
-      />
-      <img
-        alt=""
-        className="back-glow-4"
-        src="https://i.imgur.com/iWh7X3D.png"
-      />
-      <img
-        alt=""
-        className="back-glow-5"
-        src="https://i.imgur.com/iWh7X3D.png"
-      />
-      <img
-        alt=""
-        className="back-glow-6"
-        src="https://i.imgur.com/iWh7X3D.png"
-      />
-      <img
-        alt=""
-        className="back-glow-7"
-        src="https://i.imgur.com/iWh7X3D.png"
-      /> */}
     </div>
   );
 }
